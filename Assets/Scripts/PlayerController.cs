d9462c3403c044352906ae0e6f34ffb0b3e23512
kp4ws <kentpawson123@gmail.com>
2023-05-20 12:11:32 -0600

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Handle player movement
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
